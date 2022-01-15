480d2b54e17cd241b9858fe9c4eee6a6920edd0e
Chanhee Oh <77625823+kozeldark@users.noreply.github.com>
2022-01-15 15:02:10 +0900

    Commit

---
+++ package/WordRealy.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+class WordRealy extends Component{
+    state = {
+
+    };
+
+    render(){
+
+    }
+}
+
+module.exports = WordRealy;
---
+++ package/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react')
+const ReactDom = require('react-dom')
+
+const WordRealy = require('./WordRealy');
+
+ReactDom.render(<WordRealy />, document.querySelector('#root'));
